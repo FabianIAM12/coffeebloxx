b0c24469c5a8cac7150919ac147ae6c16b8e28b8
fbuehler <fbuehler@gleason.com>
2021-10-29 15:05:50 +0200

    added contract functionality and enabled frontend

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -1,9 +1,20 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "catalog ship oil armed license illegal bulb crazy elite judge vibrant daughter";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/97f5c60920c94c89b072b7eeef4cc9c8");
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
     }
   }
 };
